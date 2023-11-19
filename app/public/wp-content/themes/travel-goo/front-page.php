4cce78ff04d56cb4dacb727e48d4cc8f679b36c5
devnosa <kdevnosadze@yahoo.com>
2023-11-19 22:25:23 +0100

    more changes

--- app/public/wp-content/themes/travel-goo/front-page.php
+++ app/public/wp-content/themes/travel-goo/front-page.php
@@ -6,29 +6,29 @@ get_header();
 	<main id="primary" class="home-page">
 	<section class="home-section-4">
     <div class="container">
+	<h2 class="secondary-title">Our Popular Destinations</h2>
 		<div class="section-content">
-        <h2 class="secondary-title">Our Popular Destinations</h2>
 		<img src="<?php echo get_template_directory_uri() . '/assets/images/world-map.png'?>" alt="WORLD MAP" >
-		<div class="card">
-			<div class="card-content">
+		<div class="destinations-card">
+			<div class="destinations-card-content">
 			<img src="<?php echo get_template_directory_uri() . '/assets/images/thailand.png'?>" alt="THILAND" >	
-			<h4 class="card-title">Thailand</h4>
-			<p class="card-description">20+ Spots <br>2D & 3N</p>
-			<img src="<?php echo get_template_directory_uri() . '/assets/images/Vector.png'?>" alt="VECTOR" >	
+			<h4 class="destinations-card-title">Thailand</h4>
+			<p class="destinations-card-description">20+ Spots <br>2D & 3N</p>
+			<p class="vector"> ></p>	
 			</div>
-		<div class="card">
-			<div class="card-content">
+		<div class="destinations-card">
+			<div class="destinations-card-content">
 			<img src="<?php echo get_template_directory_uri() . '/assets/images/indonesia.png'?>" alt="INDONESIA" >	
-			<h4 class="card-title">Thailand</h4>
-			<p class="card-description">25+ Spots<br>3D & 3N</p>
-			<img src="<?php echo get_template_directory_uri() . '/assets/images/Vector.png'?>" alt="VECTOR" >	
+			<h4 class="destinations-card-title">Thailand</h4>
+			<p class="destinations-card-description">25+ Spots<br>3D & 3N</p>
+			<p class="vector"> ></p>
 			</div>
-		<div class="card">
-			<div class="card-content">
+		<div class="destinations-card">
+			<div class="destinations-card-content">
 			<img src="<?php echo get_template_directory_uri() . '/assets/images/new-zeland.png'?>" alt="NEW ZELAND" >	
-			<h4 class="card-title">Thailand</h4>
-			<p class="card-description">20+ Spots<br>3D & 2N</p>
-			<img src="<?php echo get_template_directory_uri() . '/assets/images/Vector.png'?>" alt="VECTOR" >	
+			<h4 class="destinations-card-title">Thailand</h4>
+			<p class="destinations-card-description">20+ Spots<br>3D & 2N</p>
+			<p class="vector"> ></p>
 			</div>	
 		</div>
 
@@ -39,5 +39,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
