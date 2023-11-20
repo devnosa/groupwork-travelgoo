2e66b8325c4262bf28d31d2d92f4004d11cb22e2
devnosa <kdevnosadze@yahoo.com>
2023-11-20 12:32:12 +0100

    php loop

--- app/public/wp-content/themes/travel-goo/front-page.php
+++ app/public/wp-content/themes/travel-goo/front-page.php
@@ -9,35 +9,49 @@ get_header();
 	<h2 class="secondary-title">Our Popular Destinations</h2>
 		<div class="section-content">
 		<img src="<?php echo get_template_directory_uri() . '/assets/images/world-map.png'?>" alt="WORLD MAP" >
-		<div class="destinations-card">
-			<div class="destinations-card-content">
-			<img src="<?php echo get_template_directory_uri() . '/assets/images/thailand.png'?>" alt="THILAND" >	
-			<h4 class="destinations-card-title">Thailand</h4>
-			<p class="destinations-card-description">20+ Spots <br>2D & 3N</p>
-			<p class="vector"> ></p>	
-			</div>
-		<div class="destinations-card">
-			<div class="destinations-card-content">
-			<img src="<?php echo get_template_directory_uri() . '/assets/images/indonesia.png'?>" alt="INDONESIA" >	
-			<h4 class="destinations-card-title">Indonesia</h4>
-			<p class="destinations-card-description">25+ Spots<br>3D & 3N</p>
-			<p class="vector"> ></p>
-			</div>
-		<div class="destinations-card">
-			<div class="destinations-card-content">
-			<img src="<?php echo get_template_directory_uri() . '/assets/images/new-zeland.png'?>" alt="NEW ZELAND" >	
-			<h4 class="destinations-card-title">New-Zeland</h4>
-			<p class="destinations-card-description">20+ Spots<br>3D & 2N</p>
-			<p class="vector"> ></p>
-			</div>	
-		</div>
+		<div class="destinations-cards">
 
-		
+                    <?php
+                    // Example array of destinations
+                    $destinations = array(
+                        array(
+                            'name' => 'Thailand',
+                            'image' => 'thailand.png',
+                            'description' => '20+ Spots <br>2D & 3N',
+                        ),
+                        array(
+                            'name' => 'Indonesia',
+                            'image' => 'indonesia.png',
+                            'description' => '25+ Spots<br>3D & 3N',
+                        ),
+                        array(
+                            'name' => 'New Zealand',
+                            'image' => 'new-zeland.png',
+                            'description' => '20+ Spots<br>3D & 2N',
+                        ),
+                    );
+
+                    // Loop through destinations
+                    foreach ($destinations as $destination) {
+                        ?>
+                        <div class="destinations-card">
+                            <div class="destinations-card-content">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/images/' . $destination['image']; ?>" alt="<?php echo $destination['name']; ?>">
+                                <h4 class="destinations-card-title"><?php echo $destination['name']; ?></h4>
+                                <p class="destinations-card-description"><?php echo $destination['description']; ?></p>
+                                <p class="vector">></p>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
+
+                </div>
+            </div>
         </div>
-		</div>
-</section>
-	</main><!-- #main -->
+    </section>
+</main>
 
 <?php
-
 get_footer();
+?>
